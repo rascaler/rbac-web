8e289eb4ec7d36fbf6fda2f53eb08cdb8130f8ef
rascaler <rascaler@163.com>
2017-05-15 18:03:58 +0800

    【修改】修改主页

--- src/App.vue
+++ src/App.vue
@@ -7,52 +7,36 @@
 </style>
 <template>
   <div class="container">
+    <el-row style="height: 40px">
+      <el-col :span="5"><div class="grid-content bg-purple">LOGO</div></el-col>
+      <el-col :span="19"><div class="grid-content bg-purple-light">RBAC管理</div></el-col>
+    </el-row>
+    <el-row :style="contentStyle">
+      <el-col :span="5">
+        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>导航一</template>
 
-    <nav>
-      <!-- 导航栏  -->
-      <div class="logo">
-        LOGO
-      </div>
-      <div class="nav-menu" :style="navMenuStyle">
-        <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-menu-item index="2">处理中心</el-menu-item>
-          <el-menu-item index="3">订单管理</el-menu-item>
+            <el-menu-item index="1-1"><router-link to="/example" style="display: block;width: 100%;text-decoration: none;color: black;">选项1</router-link></el-menu-item>
+            <el-menu-item index="1-2"><router-link to="/organization" style="display: block;width: 100%;text-decoration: none;color: black;">选项2</router-link></el-menu-item>
+
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+
+            <el-menu-item index="1-1"><router-link to="/test" style="display: block;width: 100%;text-decoration: none;color: black;">选项1</router-link></el-menu-item>
+            <el-menu-item index="1-2"><router-link to="/test2" style="display: block;width: 100%;text-decoration: none;color: black;">选项2</router-link></el-menu-item>
+
+          </el-submenu>
+          <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
         </el-menu>
-      </div>
-    </nav>
-
-    <!-- 侧栏 -->
-    <div class="col-menu" :style="colMenuStyle">
-      <el-row class="tac" style="width: 100%;">
-        <el-col :span="8" style="width: 100%;">
-          <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-            <el-submenu index="1">
-              <template slot="title"><i class="el-icon-message"></i>导航一</template>
-
-              <el-menu-item index="1-1"><router-link to="/example" style="display: block;width: 100%;text-decoration: none;color: black;">选项1</router-link></el-menu-item>
-              <el-menu-item index="1-2"><router-link to="/organization" style="display: block;width: 100%;text-decoration: none;color: black;">选项2</router-link></el-menu-item>
-
-            </el-submenu>
-            <el-submenu index="2">
-              <template slot="title"><i class="el-icon-menu"></i>导航二</template>
-
-              <el-menu-item index="1-1"><router-link to="/test" style="display: block;width: 100%;text-decoration: none;color: black;">选项1</router-link></el-menu-item>
-              <el-menu-item index="1-2"><router-link to="/test2" style="display: block;width: 100%;text-decoration: none;color: black;">选项2</router-link></el-menu-item>
-
-            </el-submenu>
-            <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
-          </el-menu>
-        </el-col>
-      </el-row>
-    </div>
-
-    <div id="content" :style="contentStyle">
-      <router-view></router-view>
-    </div>
-    <footer>
-
-    </footer>
+      </el-col>
+      <el-col :span="19">
+        <div>
+          <router-view></router-view>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -60,18 +44,9 @@
     created: function () {
     },
     computed: {
-      navMenuStyle: function () {
-        let width = (this.totalWidth - 270 - 40) + 'px'
-        return {'height': '60px', 'width': width}
-      },
       contentStyle: function () {
-        let width = (this.totalWidth - 270 - 40) + 'px'
-        let height = (this.totalHeight - 120 - 10) + 'px'
-        return {'height': height, 'width': width}
-      },
-      colMenuStyle: function () {
-        let height = (this.totalHeight - 120 - 10) + 'px'
-        return {'height': height, 'width': '270px'}
+        let height = (this.totalHeight - 60) + 'px'
+        return {'height': height}
       }
     },
     data: function () {
@@ -96,28 +71,6 @@
         if (!this.timer) { // 优化 因为 频繁 触发 resize 函数，导致页面很卡的 问题
           this.contentStyle['width'] = (this.totalWidth - 270 - 40) + 'px'
           this.contentStyle['height'] = (this.totalHeight - 120 - 10) + 'px'
-          this.timer = true
-          let that = this
-          setTimeout(function () {
-            that.init()
-            that.timer = false
-          }, 400)
-        }
-      },
-      navMenuStyle (val) {
-        if (!this.timer) { // 优化 因为 频繁 触发 resize 函数，导致页面很卡的 问题
-          this.navMenuStyle['width'] = (this.totalWidth - 270 - 40) + 'px'
-          this.timer = true
-          let that = this
-          setTimeout(function () {
-            that.init()
-            that.timer = false
-          }, 400)
-        }
-      },
-      colMenuStyle (val) {
-        if (!this.timer) { // 优化 因为 频繁 触发 resize 函数，导致页面很卡的 问题
-          this.colMenuStyle['height'] = (this.totalHeight - 120 - 10) + 'px'
           this.timer = true
           let that = this
           setTimeout(function () {
