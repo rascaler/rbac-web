6bc925b6c5a6475fad2a59c1b3aa005721ad70d6
rascaler <rascaler@163.com>
2017-08-03 16:12:32 +0800

    【修改】添加角色

--- src/pages/role.vue
+++ src/pages/role.vue
@@ -1,5 +1,6 @@
 <style rel="stylesheet/less" lang="less">
   @import "../styles/commons/common";
+  .el-edit-form { width: 400px;}
 </style>
 <template>
 <div id="roleContainer" class="row-mtb15">
@@ -21,7 +22,7 @@
     </el-form>
   </el-row>
   <el-row class="no-margin">
-    <el-button type="primary" @click="search">查询</el-button>
+    <el-button type="primary" @click="openEditDialog('add')">添加</el-button>
   </el-row>
   <el-row>
     <el-table
@@ -35,6 +36,7 @@
       <el-table-column label="操作">
         <template scope="scope">
           <el-button size="small" @click="operation_detail(scope.$index, scope.row)">详情</el-button>
+          <el-button size="small" @click="operation_updateState(scope.$index, scope.row)">启用</el-button>
           <el-button size="small" @click="operation_delete(scope.$index, scope.row)">删除</el-button>
         </template>
       </el-table-column>
@@ -50,6 +52,31 @@
     </el-pagination>
 
     <!-- 添加修改对话框  -->
+    <el-dialog  :title="editDialogConfig.title" :visible.sync="editDialogConfig.visible" @close="closeEditDialog">
+
+      <el-form class="el-edit-form" :model="roleFormConfig.data" :rules="roleFormConfig.rules" ref="roleForm" :label-width="roleFormConfig.style.labelWidth">
+        <el-form-item label="父角色" prop="parentId">
+          <el-select clearable v-model="roleFormConfig.data.parentId" placeholder="请选择父角色">
+            <el-option
+              v-for="item in roleFormConfig.parentRole.options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="角色名" prop="name">
+          <el-input v-model="roleFormConfig.data.name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="描述" prop="description">
+          <el-input type="textarea" v-model="roleFormConfig.data.description" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="closeEditDialog">取 消</el-button>
+        <el-button type="primary" @click="roleSaveOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
     <!-- 查看详情对话框 -->
   </el-row>
 
@@ -63,6 +90,13 @@
       pageNum: 1,
       pageSize: 10
   }
+  const roleFormInit = {
+      id: '',
+      name: '',
+      state: '',
+      description: '',
+      parentId: ''
+  }
   export default {
     name: 'role',
     mounted () {
@@ -75,8 +109,29 @@
         this.searchFormConfig.data.pageSize = this.dataGridConfig.paginationConfig.pageSize
         this.pageRoles()
       },
+      openEditDialog (type) {
+        this.editDialogConfig.visible = true
+        if (type === 'add') {
+            this.$message.success('添加')
+        } else {
+            this.$message.success('修改')
+        }
+      },
+      closeEditDialog () {
+        // 重置表单
+        this.editDialogConfig.visible = false
+      },
+      roleSaveOrUpdate () {
+//        this.$loading({target: document.getElementById('editDialog')})
+        this.$message.success('保存成功')
+        this.closeEditDialog()
+      },
       // >>>>>>>>>>>>>>>>>>dataGridConfig<<<<<<<<<<<<<<<<<<<<<<<<
       operation_detail (index, row) {
+          this.openEditDialog('update')
+      },
+      operation_updateState (index, row) {
+        this.$message.success('启用成功')
       },
       operation_delete (index, row) {
         this.$confirm('此操作将永久删除该角色, 是否继续?', '提示', {
@@ -135,7 +190,23 @@
             total: 0,
             currentPage: 1
           }
+        },
+        editDialogConfig: {
+            title: '编辑',
+            visible: false
+        },
+        roleFormConfig: {
+           data: JSON.parse(JSON.stringify(roleFormInit)),
+           rules: null,
+           style: {
+              labelWidth: '80px'
+           },
+           parentRole: {
+            default: 0,
+            options: []
+          }
         }
+
       }
     }
   }
